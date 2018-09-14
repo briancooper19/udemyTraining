821b91f63065d92c43877a80f438eba30e2775a7
Brian Cooper <brian_cooper@sbcglobal.net>
2018-09-14 00:41:15 -0500

    added New Project

---
+++ New Project/test1.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello there again")
+	//option D shows reference
+	//control R runs script
+	//command s saves and runs go fmt
+	//command shift p gives you a search window for options
+	//changes
+
+}
