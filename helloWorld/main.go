4c83fa668bc1a06703497ce0f31a84ae86d89ba0
Brian Cooper <brian_cooper@sbcglobal.net>
2018-09-14 00:20:52 -0500

    my second commit

---
+++ helloWorld/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world!! Peace!")
+	//option D shows reference
+	//control R runs script
+	//command s saves and runs go fmt
+	//command shift p gives you a search window for options
+
+}
